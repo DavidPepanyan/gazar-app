c3873ad126b48271adad2831407678250a66d12a
DavidPepanyan <dpepanyan0@gmail.com>
2026-04-15 19:28:22 +0400

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Button, Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    <View className="flex-1 items-center  justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500 bg-red-500">
+        Welcome to Nativewind!asas
+      </Text>
+
+      <Button title="Click me" onPress={() => alert("Button pressed")} />
+    </View>
+  );
+}
