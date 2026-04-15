6f591e1a6ad1cd3878a48578f45e8c228d00ddfa
DavidPepanyan <dpepanyan0@gmail.com>
2026-04-15 19:29:06 +0400

    Update text color in App component from blue to green

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Button, Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center  justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500 bg-red-500">
+      <Text className="text-xl font-bold text-green-500 bg-red-500">
         Welcome to Nativewind!asas
       </Text>
 
