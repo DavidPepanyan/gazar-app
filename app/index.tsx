b67e4488f5caeacb24919fd47cc2494512aa21f5
DavidPepanyan <dpepanyan0@gmail.com>
2026-04-17 14:31:04 +0400

    Update app.json to change icon paths and modify splash screen image; refactor App component to use new logo and improve layout with updated image handling.

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,32 @@
-import {
-    Alert,
-    Image,
-    ImageBackground,
-    Pressable,
-    Text,
-    View,
-} from "react-native";
+import { Image, ImageBackground } from "expo-image";
+import { useCallback } from "react";
+import { Alert, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import logo from "../assets/images/logos/logo.png";
 
 export default function App() {
+  const handleWelcomePress = useCallback(() => {
+    Alert.alert("Button pressed");
+  }, []);
+
   return (
     <ImageBackground
       source={require("../assets/images/OnBoarding/374.jpg")}
-      style={{ flex: 1 }}
-      resizeMode="cover"
+      className="flex-1"
+      contentFit="cover"
     >
       <View className="absolute inset-0 bg-black/30" />
-      <SafeAreaView className="flex-1 relative ">
-        <View className="absolute inset-0 items-center justify-center">
-          <Image source={logo} className="w-64 object-contain" />
+      <SafeAreaView className="relative flex-1">
+        <View className="absolute inset-0 z-10 items-center justify-center">
+          <Image
+            source={require("../assets/images/logos/logo.png")}
+            style={{ width: 180, height: 160 }}
+            contentFit="contain"
+          />
         </View>
 
         <View className="justify-end w-full h-full">
           <Pressable
-            onPress={() => Alert.alert("Button pressed")}
+            onPress={handleWelcomePress}
             className="bg-[#7ac943] px-5 py-3 rounded-3xl w-[90%] self-center items-center justify-center"
           >
             <Text className="text-white font-bold text-center text-xl">
